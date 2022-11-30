0e401ffdf7872bfe6e705af78bca6c764717bc41
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-29 23:01:27 -0800

    Add template files, add card component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+  import './App.css';
+  import Card from "./Components/Card"
+  import {nanoid} from "nanoid"
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Card (props) {
+
+    return (
+        <div className="card-container">
+            <div className="card-img-container"></div>
+            <div className="card-header">
+                <h2>Illmatic</h2>
+            </div>
+        </div>
+    )
+
+}
